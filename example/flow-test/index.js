f898fde9093a05f125263895ccf4960c781dd4b6
chenguangxiao <chenguangxiao@jd.com>
2018-09-26 00:02:50 +0800

    vue analysis

---
+++ example/flow-test/index.js
@@ -0,0 +1,27 @@
+/*@flow*/``
+
+// function split(str) {
+//     return str.split(' ')
+// }
+  
+// split(11) // Cannot call str.split because property split is missing in Number [1]
+
+/** -------- */
+
+/*@flow*/
+// function add(x, y){
+//     return x + y
+// }
+
+// add('Hello', 11) // no error
+
+
+/*@flow*/
+// function add(x: number, y: number): number {
+//     return x + y
+// }
+
+// add('Hello', 11) // Cannot call add with 'Hello' bound to x because string [1] is incompatible with number [2].
+
+
+
